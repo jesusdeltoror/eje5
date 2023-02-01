acb5bca99b090f41d006fa02dfa5a748ac10bb73
jesusdeltoror <jesusdeltoror@gmail.com>
2023-01-31 19:42:10 -0600

    eje5

---
+++ index.js
@@ -0,0 +1,13 @@
+let alturaCm = 180;
+let alturaM = 1.8;
+let pesoKg = 100;
+pesoKg = pesoKg.toPrecision(5);
+let alturaRedondeadaM = Math.round(alturaM);
+let pesoRedondeadaKg = Math.floor(pesoKg);
+let max = Number.MAX_VALUE + 1 === Number.MAX_VALUE;
+
+
+console.log(pesoKg);
+console.log(alturaRedondeadaM);
+console.log(pesoRedondeadaKg);
+console.log(max);
